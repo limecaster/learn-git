54947e75b8edeebd491d0fb00823114163f3baa9
limecaster <bang.dovan38@gmail.com>
2025-08-06 13:46:03 +0700

    add bar.cs

---
+++ bar.cs
@@ -0,0 +1,13 @@
+namespace MyNamespace
+{
+    public class MyClass
+    {
+        public void MyMethod()
+        {
+            Console.WriteLine("Hello, World!");
+            
+            var currentTime = DateTime.Now;
+            Console.WriteLine($"Current time is: {currentTime}");
+        }
+    }
+}
